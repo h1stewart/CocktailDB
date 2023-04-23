28c5d494d5d4f09fb4b2b77a271b2d6629ada479
Hailey Stewart <kickinhailey@icloud.com>
2023-04-23 13:03:53 -0400

    Initial Commit

---
+++ PersonalApp/PersonalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalAppApp.swift
+//  PersonalApp
+//
+//  Created by Hailey Stewart on 4/23/23.
+//
+
+import SwiftUI
+
+@main
+struct PersonalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
